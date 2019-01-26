4a5f93ae078d769ba89f5802bfefeae173018e36
Prashant Morem <prashantmorem@knovator.com>
2019-01-26 15:14:56 +0530

    user action information added

--- config/http-logger.php
+++ config/http-logger.php
@@ -3,14 +3,22 @@
 return [
 
     /*
-     * Filter out body fields which will never be logged.
-     */
-    'except' => [
+    * Filter out body fields which will never be logged.
+    */
+    'except'      => [
         'password',
         'password_confirmation',
     ],
 
     /* Default log channel.*/
     'log_channel' => 'custom_log',
+
+    'action_by_columns' => [
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'phone'
+    ],
 
 ];
--- src/DefaultLogWriter.php
+++ src/DefaultLogWriter.php
@@ -16,11 +16,11 @@ class DefaultLogWriter implements LogWriter
      * @param Request $request
      * @return mixed|void
      */
-    public function logRequest(Request $request)
-    {
+    public function logRequest(Request $request) {
         $method = strtoupper($request->getMethod());
 
         $uri = $request->getPathInfo();
+
 
         $bodyAsJson = json_encode($request->except(config('http-logger.except')));
 
@@ -28,10 +28,19 @@ class DefaultLogWriter implements LogWriter
             return $file->getClientOriginalName();
         }, iterator_to_array($request->files));
 
-        $message = "{$method} {$uri} - Body: {$bodyAsJson} - Files: " . implode(', ', $files);
+        $message = "{$method} {$uri} - Body: {$bodyAsJson}";
+        if (!empty(implode(', ', $files))) {
+            $message .= " - Files: \" . implode(', ', $files)";
+        }
+
+        if (auth()->guard('api')->check()) {
+            $user = auth()->guard('api')->user()->first(config('http-logger.action_by_columns'))->toArray();
+            $userBody = json_encode($user);
+            $message .= " - Action By: {$userBody}";
+        }
 
         $channel = config('http-logger.log_channel');
-        
+
         Log::channel($channel)->info($message);
     }
 }
