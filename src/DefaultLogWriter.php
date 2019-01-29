0c3862291538966ab95096bc1e91f0ce4c205afd
Prashant Morem <prashantmorem@knovator.com>
2019-01-29 16:44:35 +0530

    multiple file error changed

--- src/DefaultLogWriter.php
+++ src/DefaultLogWriter.php
@@ -12,29 +12,26 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
  */
 class DefaultLogWriter implements LogWriter
 {
+
     /**
      * @param Request $request
      * @return mixed|void
      */
     public function logRequest(Request $request) {
+        $fileNames = [];
         $method = strtoupper($request->getMethod());
-
         $uri = $request->getPathInfo();
-
-
         $bodyAsJson = json_encode($request->except(config('http-logger.except')));
-
-        $files = array_map(function (UploadedFile $file) {
-            return $file->getClientOriginalName();
-        }, iterator_to_array($request->files));
-
+        $this->uploadedFiles($request->files, $fileNames);
         $message = "{$method} {$uri} - Body: {$bodyAsJson}";
-        if (!empty(implode(', ', $files))) {
-            $message .= " - Files: " . implode(', ', $files);
+        dd($fileNames);
+        if (!empty($fileNames)) {
+            $message .= " - Files: " . implode(', ', $fileNames);
         }
 
         if (auth()->guard('api')->check()) {
-            $user = auth()->guard('api')->user()->first(config('http-logger.action_by_columns'))->toArray();
+            $user = auth()->guard('api')->user()->first(config('http-logger.action_by_columns'))
+                          ->toArray();
             $userBody = json_encode($user);
             $message .= " - Action By: {$userBody}";
         }
