199d104647964b420440db2b96edc0ad0b0f6810
Prashant Morem <prashantmorem@knovator.com>
2019-02-14 16:45:38 +0530

    user agent information added

--- src/DefaultLogWriter.php
+++ src/DefaultLogWriter.php
@@ -3,6 +3,7 @@
 namespace Knovators\HttpLogger;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
@@ -21,11 +22,12 @@ class DefaultLogWriter implements LogWriter
         $fileNames = [];
         $method = strtoupper($request->getMethod());
         $uri = $request->getPathInfo();
-        $bodyAsJson = json_encode($request->except(config('http-logger.except')));
+        $bodyAsJson = json_encode($this->input($request, config('http-logger.except')));
+        $message = "{$method} {$uri} - Action From: {$this->clientInformation($request)} - Body: {$bodyAsJson}";
         $this->uploadedFiles($request->files, $fileNames);
-        $message = "{$method} {$uri} - Body: {$bodyAsJson}";
+
         if (!empty($fileNames)) {
-            $message .= " - Files: " . implode(', ', $fileNames);
+            $message .= " - Files: " . json_encode($fileNames);
         }
 
         if (auth()->guard('api')->check()) {
@@ -34,31 +36,59 @@ class DefaultLogWriter implements LogWriter
             $userBody = json_encode($user);
             $message .= " - Action By: {$userBody}";
         }
+        
 
         $channel = config('http-logger.log_channel');
 
         Log::channel($channel)->info($message);
     }
 
+    /**
+     * Get all of the input except for a specified array of items.
+     *
+     * @param              $request
+     * @param  array|mixed $expectKeys
+     * @return array
+     */
+    private function input($request, $expectKeys) {
+        /** @var Request $request */
+        $results = $request->input();
+        Arr::forget($results, $expectKeys);
+
+        return $results;
+    }
 
     /**
-     * @param $requestFiles
-     * @param $fileNames
+     * @param $request
+     * @return mixed
      */
-    private function uploadedFiles($requestFiles, &$fileNames) {
+    private function clientInformation($request) {
+        /** @var Request $request */
+        $response['agent'] = $request->header('user-agent');
+        $response['ip'] = $request->ip();
+        return json_encode($response);
+
+    }
+
+    /**
+     * @param      $requestFiles
+     * @param      $fileNames
+     * @param bool $parentKey
+     */
+    private function uploadedFiles($requestFiles, &$fileNames, $parentKey = false) {
         if (!is_array($requestFiles)) {
             $requestFiles = iterator_to_array($requestFiles);
         }
-        array_map(function ($files) use (&$fileNames) {
-            if (is_array($files)) {
-                $this->uploadedFiles($files, $fileNames);
+        foreach ($requestFiles as $fileKey => $file) {
+            if (is_array($file)) {
+                $this->uploadedFiles($file, $fileNames, $fileKey);
+            } elseif ($parentKey) {
+                $fileNames[$parentKey][$fileKey] = $file->getClientOriginalName();
             } else {
-                /** @var UploadedFile $files */
-                array_push($fileNames,$files->getClientOriginalName());
-
+                $fileNames[$fileKey] = $file->getClientOriginalName();
             }
 
-        }, $requestFiles);
+        }
 
     }
 }
